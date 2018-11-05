d96cda510d8d3fb8fd0f5f99f4fcfa82b6f8e637
Wilhelm Behncke <behncke@sitegeist.de>
2018-11-05 18:36:47 +0100

    FEATURE: Implemen Role Query Type

--- Classes/GraphQl/Query/Role.php
+++ Classes/GraphQl/Query/Role.php
@@ -13,9 +13,10 @@ namespace Neos\Neos\Ui\GraphQl\Query;
 
 use GraphQL\Type\Definition\ObjectType;
 use Neos\Neos\Ui\GraphQl\Type\Type;
+use Neos\Flow\Security\Policy;
 
 /**
- * @TODO: Class Comment
+ * GraphQl representation of a flow security role
  */
 class Role extends ObjectType
 {
@@ -27,10 +28,64 @@ class Role extends ObjectType
     public function __construct(array $configuration = [])
     {
         return parent::__construct(array_merge([
-            'name' => '', // @TODO: name
-            'description' => '', // @TODO: description
+            'name' => 'Role',
+            'description' => 'A security role',
         ], $configuration, [
-            // @TODO: implementation
-        ]))
+            'identifier' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'The role identifier',
+                'resolve' => function (Policy\Role $role) {
+                    return $role->getIdentifier();
+                }
+            ],
+            'packageKey' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'The role package key',
+                'resolve' => function (Policy\Role $role) {
+                    return $role->getPackageKey();
+                }
+            ],
+            'name' => [
+                'type' => Type::nonNull(Type::string()),
+                'description' => 'The role name',
+                'resolve' => function (Policy\Role $role) {
+                    return $role->getName();
+                }
+            ],
+            'isAbstract' => [
+                'type' => Type::boolean(),
+                'description' => 'Indicates whether the role is abstract',
+                'resolve' => function (Policy\Role $role) {
+                    return $role->isAbstract();
+                }
+            ],
+            'parentRoles' => [
+                'type' => Type::listOf(Type::role()),
+                'description' => 'The parent roles of this role',
+                'resolve' => function (Policy\Role $role) {
+                    return $role->getParentRoles();
+                }
+            ],
+            'allParentRoles' => [
+                'type' => Type::listOf(Type::role()),
+                'description' => 'All parent roles of this role',
+                'resolve' => function (Policy\Role $role) {
+                    return $role->getAllParentRoles();
+                }
+            ],
+            'hasParentRole' => [
+                'type' => Type::listOf(Type::role()),
+                'description' => 'Indicates whether this role has the given parent role',
+                'args' => [
+                    'parentRoleIdentifier' => [
+                        'type' => Type::nonNull(Type::string()),
+                        'description' => 'The identifier of the parent role'
+                    ]
+                ],
+                'resolve' => function (Policy\Role $role, array $arguments) {
+                    return $role->hasParentRole(arguments['parentRoleIdentifier']);
+                }
+            ]
+        ]));
     }
 }
